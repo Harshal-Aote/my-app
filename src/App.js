1be94af9c123c3ec17be367feed8aec1681bf362
Harshal-Aote <harshalaote11@gmail.com>
2023-05-18 17:18:58 +0530

    My 4th Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import Add from "./component/Add/Add";
 
 function App() {
   return (
     <div>
       <h1>First React Project</h1>
       <p>This is p tag</p>
+      <Add></Add>
     </div>
   );
 }
---
+++ src/component/Add/Add.js
@@ -0,0 +1,9 @@
+const Add = () =>{
+    return(
+        <div>
+            <p>This is Add Component</p>
+        </div>
+    )
+}
+
+export default Add;
